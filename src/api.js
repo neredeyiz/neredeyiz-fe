7bf0633ca90ddf431504828560230944053f12f2
neredeyiz <ruffle461@windowslive.com>
2025-03-30 14:20:17 +0300

    Get api url from env

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 export const fetchChoices = async () => {
   try {
